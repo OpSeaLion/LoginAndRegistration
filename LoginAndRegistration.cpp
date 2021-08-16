5f386ce6b9602a938beab9cd4843114a3ecbaade
Lester Kong <44414749+OpSeaLion@users.noreply.github.com>
2021-08-15 22:58:05 -0400

    Add files via upload

--- LoginAndRegistration.cpp
+++ LoginAndRegistration.cpp
@@ -6,8 +6,19 @@ int main(int argc, char* argv[])
 	std::string username = "Raymond";
 	int password = 1234;
 
-	std::string 
+	std::string enteredUsername;
+	int enteredPassword = 0;
 
-	cout << "Enter username: ";
-	cin >> 
+	std::cout << "Enter username: ";
+	std::cin >> enteredUsername;
+
+	std::cout << "\n" << "Enter password: ";
+	std::cin >> enteredPassword;
+
+	std:: cout << "\n";
+
+	if(enteredPassword != password || enteredUsername != username)
+		std::cout << "Access Denied.";
+	else
+		std::cout << "Access Granted.";
 }
