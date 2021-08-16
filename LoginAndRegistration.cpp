d51216520698a730d84bd179f0a6e070a4098f4f
Lester Kong <44414749+OpSeaLion@users.noreply.github.com>
2021-08-15 22:39:15 -0400

    Add files via upload

---
+++ LoginAndRegistration.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+	std::string username = "Raymond";
+	int password = 1234;
+
+	std::string 
+
+	cout << "Enter username: ";
+	cin >> 
+}
